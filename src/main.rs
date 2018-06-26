f65c9b8ca50225930b816b1eb2a70d9dc1ac8441
Drakeor <drakeor.dragon@gmail.com>
2018-06-26 00:26:53 -0700

    Framework built and AI functions now run

--- src/main.rs
+++ src/main.rs
@@ -4,56 +4,62 @@ use std::io;
 use rand::Rng;
 use std::collections::VecDeque;
 
-const MAXHEALTH : i32 = 100;
-const ACTIONS_PER_TURN : usize = 5;
+const MAXHEALTH: i32 = 100;
+const ACTIONS_PER_TURN: usize = 5;
 const BASE_INCOME: i32 = 10;
-const BASE_DEVELOPMENT_COST : i32 = 100;
-const BASE_DEVELOPMENT_INCREMENT : f32 = 1.5;
+const BASE_DEVELOPMENT_COST: i32 = 100;
+const BASE_DEVELOPMENT_INCREMENT: f32 = 1.5;
 
 // Careers a player can have
 #[derive(Debug)]
 enum Career {
     Politician,
     Soldier,
-    Craftsman
+    Craftsman,
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum TurnTask {
     Work,
     Develop,
     Socialize,
     Attack,
     Train,
-    Office
+    Office,
+}
+
+#[derive(Debug, PartialEq)]
+enum AIType {
+    Player,
+    BasicWorkAI
 }
 
 struct TurnAction {
-    turn_task : TurnTask,
-    target : i32
+    turn_task: TurnTask,
+    target: i32,
 }
 
 // Represents an office position
 struct Office {
     name: String,
     level: i32,
-    salary: i32
+    salary: i32,
 }
 
 // Represents a player in the guild
 struct Player {
     health: i32,
-    money : i32,
-    skill_level : i32,
-    develop_level : i32,
+    money: i32,
+    skill_level: i32,
+    develop_level: i32,
     disposition: Career,
-    combat_level : i32
+    combat_level: i32,
+    turn_func: fn(c_player: usize, players: &Vec<Player>) -> VecDeque<TurnAction>,
+    ai_type: AIType
 }
 
 // Generates a random player
-fn GeneratePlayer() -> Player {
-
+fn GeneratePlayer(ai_type: AIType) -> Player {
     let career = match rand::thread_rng().gen_range(0_i32, 3_i32) {
         0 => Career::Politician,
         1 => Career::Soldier,
@@ -66,7 +72,12 @@ fn GeneratePlayer() -> Player {
         skill_level: 1,
         develop_level: 1,
         disposition: career,
-        combat_level : 1
+        combat_level: 1,
+        turn_func: match ai_type {
+            AIType::Player => do_player,
+            _ => do_ai_basic
+        },
+        ai_type: ai_type
     };
 
     player
@@ -82,17 +93,19 @@ fn action_exists(l_task: TurnTask, queue: &VecDeque<TurnAction>) -> bool {
     false
 }
 
+// Returns true or false if the item can be afforded
 fn can_afford(bank: i32, cost: i32) -> bool {
     cost <= bank
 }
 
 // How much money is earned from working
 fn calc_work(player: &Player) -> i32 {
-    let mut w = BASE_INCOME + ((player.develop_level + 1_i32) / 2_i32) * (player.skill_level / 5_i32); 
+    let mut w =
+        BASE_INCOME + ((player.develop_level + 1_i32) / 2_i32) * (player.skill_level / 5_i32);
     w = match player.disposition {
         Career::Politician => (w as f32 * 0.9_f32) as i32,
         Career::Craftsman => (w as f32 * 1.1_f32) as i32,
-        _ => w
+        _ => w,
     };
     w
 }
@@ -102,98 +115,141 @@ fn calc_develop(player: &Player) -> i32 {
     (BASE_DEVELOPMENT_COST as f32 * BASE_DEVELOPMENT_INCREMENT.powi(player.develop_level)) as i32
 }
 
+// Player Function
+// Player controls this person
+fn do_player(c_player: usize, players: &Vec<Player>) -> VecDeque<TurnAction> {
+    let mut taskBuf = VecDeque::new();
+
+    // Queue up actions
+    while taskBuf.len() < ACTIONS_PER_TURN {
+        // Get input
+        println!("Action {} / {}", taskBuf.len() + 1_usize, ACTIONS_PER_TURN);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input);
+
+        // Process input
+        match input.trim() {
+            "1" => taskBuf.push_front(TurnAction {
+                turn_task: TurnTask::Work,
+                target: 0,
+            }),
+            "2" => {
+                if action_exists(TurnTask::Develop, &taskBuf) {
+                    println!("Cannot develop more than once per turn!");
+                    continue;
+                }
+                taskBuf.push_front(TurnAction {
+                    turn_task: TurnTask::Develop,
+                    target: 0,
+                });
+            }
+            "7" => {
+                println!("Stats");
+                for i in 0..8 {
+                    println!(
+                        "Player {}: ${} | {} HP | {} Combat | {} Skill | {} Develop | {:?} | {:?}",
+                        i,
+                        players[i].money,
+                        players[i].health,
+                        players[i].combat_level,
+                        players[i].skill_level,
+                        players[i].develop_level,
+                        players[i].disposition,
+                        players[i].ai_type
+                    );
+                }
+            }
+
+            _ => {
+                println!("Invalid option or not implemented yet: {}", input);
+            }
+        }
+    }
+
+    taskBuf
+}
+
+// AI Function
+// Selects actions for that player's turn
+fn do_ai_basic(c_player: usize, players: &Vec<Player>) -> VecDeque<TurnAction> {
+    let mut taskBuf = VecDeque::new();
+    while taskBuf.len() < ACTIONS_PER_TURN {
+        // This AI is stupid and works all day
+        taskBuf.push_back(TurnAction {
+            turn_task: TurnTask::Work,
+            target: 0,
+        });
+    }
+    taskBuf
+}
+
 // Entry point
 fn main() {
-    
     let mut players = Vec::new();
     let mut is_running = true;
 
-    for _ in 0..8 {
-        players.push(GeneratePlayer());
+    players.push(GeneratePlayer(AIType::Player));
+    for _ in 1..8 {
+        players.push(GeneratePlayer(AIType::BasicWorkAI));
     }
 
-            println!("");
-        println!("Guild Sim");
-        println!("Goal: Hold the highest political office for 5 turns");
-        println!("");
+    println!("");
+    println!("Guild Sim");
+    println!("Goal: Hold the highest political office for 5 turns");
+    println!("");
 
     while is_running {
-
-        let mut taskBuf = VecDeque::new();
+        let mut taskBuf : VecDeque<TurnAction> = VecDeque::new();
 
         // Print menu
         println!("");
         println!("Options (Bank: ${})", players[0].money);
         println!("1 - Produce Goods (+${})", calc_work(&players[0]));
-        println!("2 - Develop Guild (-${}, +Development)", calc_develop(&players[0]));
+        println!(
+            "2 - Develop Guild (-${}, +Development)",
+            calc_develop(&players[0])
+        );
         println!("3 - Socialize with Player");
         println!("4 - Attack Player");
         println!("5 - Train");
         println!("6 - Run for Office");
         println!("7 - Stats");
 
-        // Queue up actions
-        while taskBuf.len() < ACTIONS_PER_TURN {
-
-            // Get input
-            println!("Action {} / {}", taskBuf.len() + 1_usize, ACTIONS_PER_TURN);
-            let mut input = String::new();
-            io::stdin().read_line(&mut input);
-
-            // Process input 
-            match input.trim() {
-                "1" => taskBuf.push_front(TurnAction { turn_task: TurnTask::Work, target: 0 } ),
-                "2" => {
-                    if action_exists(TurnTask::Develop, &taskBuf) {
-                        println!("Cannot develop more than once per turn!");
-                        continue;
-                    }
-                    taskBuf.push_front(TurnAction { turn_task: TurnTask::Develop, target: 0 } );
-                }
-                "7" => {
-                    println!("Stats");
-                    for i in 0..8 {
-                        println!("Player {}: ${} | {} HP | {} Combat | {} Skill | {} Develop | {:?}", i, 
-                            players[i].money, players[i].health, 
-                            players[i].combat_level, players[i].skill_level, players[i].develop_level, players[i].disposition);
-                    }
-                }
-
-                _ => {
-                    println!("Invalid option or not implemented yet: {}", input);
-                }
-            }
-        }
-
         // Process actions
-        // Player 0 is us
-        let c_player = 0; 
-        while !taskBuf.is_empty() {
-            let elem = taskBuf.pop_back().unwrap();
-            match elem.turn_task {
-                TurnTask::Work => {
-                    let income = calc_work(&players[c_player]);
-                    players[c_player].money += income;
-                    players[c_player].skill_level += 1;
-                    println!("Player {} works, earning {} income.", c_player, income);
-                }
-                TurnTask::Develop => {
-                    let cost = calc_develop(&players[c_player]);
-                    if !can_afford(players[c_player].money, cost) {
-                        println!("Player {} cannot afford development. It costs {} and they have {}.", c_player, cost, players[c_player].money);
-                        continue;
+        for c_player in 0..8 {
+            // Run AI or player turn task to get actions
+            let mut taskBuf = (players[c_player].turn_func)(c_player, &players);
+
+            // Process each action for this player
+            while !taskBuf.is_empty() {
+                let elem = taskBuf.pop_back().unwrap();
+                match elem.turn_task {
+                    // Working is the simpliest task
+                    TurnTask::Work => {
+                        let income = calc_work(&players[c_player]);
+                        players[c_player].money += income;
+                        players[c_player].skill_level += 1;
+                        println!("Player {} works, earning {} income.", c_player, income);
                     }
-                    players[c_player].money -= cost;
-                    players[c_player].develop_level += 1;
-                    println!("Player {} upgrades guild to level {}. They have {} left.", c_player,  players[c_player].develop_level, players[c_player].money);
-                }
-                _ => {
-                    println!("Failed to process unknown action {:?}", elem.turn_task);
+                    // Develops the guild the player owns
+                    TurnTask::Develop => {
+                        let cost = calc_develop(&players[c_player]);
+                        if !can_afford(players[c_player].money, cost) {
+                            println!("Player {} cannot afford development. It costs {} and they have {}.", c_player, cost, players[c_player].money);
+                            continue;
+                        }
+                        players[c_player].money -= cost;
+                        players[c_player].develop_level += 1;
+                        println!(
+                            "Player {} upgrades guild to level {}. They have {} left.",
+                            c_player, players[c_player].develop_level, players[c_player].money
+                        );
+                    }
+                    _ => {
+                        println!("Failed to process unknown action {:?}", elem.turn_task);
+                    }
                 }
             }
         }
-
-
     }
-
 }
